60a0737b03f7a2671beed8816c710ad739227cde
Islem Mannai <islemmannaiii@gmail.com>
2022-03-03 22:48:26 +0100

    test

--- src/ecole/EspritHashSet.java
+++ src/ecole/EspritHashSet.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +11,7 @@ import java.util.Set;
 
 /**
  *
- * @author DELL
+ * @author islem mannai
  */
 public class EspritHashSet  implements GestionEnseignant{
 
