137ab6c63d2ce671296e2b06af23175c697f653e
Rahma Oueslati <rahma.oueslati@esprit.tn>
2022-03-04 11:10:28 +0100

    modif 2

--- src/ecole/EspritArrayList.java
+++ src/ecole/EspritArrayList.java
@@ -61,4 +61,16 @@ public class EspritArrayList  implements University {
        Collections.sort(list, Etudiant.ComparatorNom);
     }
     
+    public static void main(String[] args) {
+        Etudiant e= new Etudiant(1,"Ahmed","Oueslati");
+        Etudiant e1= new Etudiant(0,"Ahmed","Oueslati");
+        Etudiant e2= new Etudiant(3,"Ahmed","Oueslati");
+        EspritVector ecole=new EspritVector();
+        ecole.ajouterEtudiant(e);
+        ecole.ajouterEtudiant(e1);
+        ecole.ajouterEtudiant(e2);
+        ecole.trierEtudiantsParId();
+        ecole.displayEtudiants();
+    }
+
 }
